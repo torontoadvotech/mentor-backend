82bd3e23f911c0e391ef8f0628badbb34e171b92
Dennis <dennis.mcleod92@gmail.com>
2020-03-14 22:24:42 -0400

    user model updates

--- app.js
+++ app.js
@@ -44,14 +44,7 @@ app.use(xss());
 // Prevent parameter pollution
 app.use(
   hpp({
-    whitelist: [
-      'duration',
-      'ratingsQuantity',
-      'ratingsAverage',
-      'maxGroupSize',
-      'difficulty',
-      'price'
-    ]
+    whitelist: []
   })
 );
 
